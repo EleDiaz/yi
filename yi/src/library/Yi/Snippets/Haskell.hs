b64eb1c067ad8788762f6fc941c90ac836e1c7aa
Eleazar Diaz Delgado <eletodoguaysinc@gmail.com>
2013-07-12 15:48:06 +0100

    new snippets for haskell

--- yi/src/library/Yi/Snippets/Haskell.hs
+++ yi/src/library/Yi/Snippets/Haskell.hs
@@ -10,3 +10,28 @@ hsFunction = snippet $
 hsClass :: SnippetCmd ()
 hsClass = snippet $
   "class " & (cursor 1) & " " & (cursor 2) & " where\n  " & (cursor 3)
+
+hsPragma :: SnippetCmd ()
+hsPragma = snippet $ "{-# LANGUAGE " & cursor 1 & " #-}"
+
+hsComment :: SnippetCmd ()
+hsComment = snippet $ "{- " & cursor 1 & " -}"
+
+hsData :: SnippetCmd ()
+hsData = snippet $ "data " & cursor 1 & " = " & cursor 2
+
+hsNewtype :: SnippetCmd ()
+hsNewtype = snippet $ "newtype " & cursor 1 & " = " & cursor 2
+
+hsType :: SnippetCmd ()
+hsType = snippet $ "type " & cursor 1 & " = " & cursor 2
+
+hsSection :: SnippetCmd ()
+hsSection = snippet $ (replicate 80 '-' ++ "\n-- ") & cursor 1 & ("\n" ++ (replicate 80 '-'))
+
+hsModule :: SnippetCmd ()
+hsModule = snippet $ "module " & cursor 1 & " where" & cursor 2
+
+hsInit :: SnippetCmd ()
+hsInit = snippet $ 
+          (replicate 80 '-' ++ "\n-- Module      : ") & cursor 1 & "\n-- Copyright   : " & cursor 2 & "\n-- License     : " & cursor 3 & "\n--\n-- Maintainer  : " & cursor 4 & "\n-- Stability   : " & cursor 5 & "\n-- Portability : " & cursor 6 & "\n--\n-- | " & cursor 7 & ("\n--\n" ++ (replicate 80 '-'))
